4e81f0387848a6f3ac1dfeb140ddf3f2d138b338
r.j <rjpe@protonmail.com>
2019-03-22 23:06:52 -0400

    Implements part of Issue #10 Implements movement functionality for the Rook class.

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -14,10 +14,41 @@ public class Rook extends BoardPiece {
 
     @Override
     public boolean getPotentialMoves(Board chess) {
-        return true;
-    }
+        moves.clear();
+        // TODO add checkForEnemy() function to limit the number of spaces that piece can move.
 
-    public boolean isValidMove(Point requestedMove) {return true;}
+        int x_pos = this.getCurrentSpace().getPosition().x;
+        int y_pos =  this.getCurrentSpace().getPosition().y;
+
+        // loop through spaces to the left of the piece
+        for(int i = x_pos; i >= 0; i--){
+            if (!checkForFriend(chess.board[y_pos][i])){
+                moves.add(new Point(i, y_pos));
+            }
+        }
+
+        // loop through spaces to the right of the piece
+        for(int i = x_pos; i < 8; i++){
+            if (!checkForFriend(chess.board[y_pos][i])){
+                moves.add(new Point(i, y_pos));
+            }
+        }
+
+        // loop through spaces in front of the piece
+        for (int i = y_pos; i < 8; i++){
+            if (!checkForFriend(chess.board[i][x_pos])){
+                moves.add(new Point(x_pos, i));
+            }
+        }
+
+        // loop through spaces behind piece
+        for(int i = y_pos; i >= 0; i-- ){
+            if (!checkForFriend(chess.board[i][x_pos])){
+                moves.add(new Point(x_pos, i));
+            }
+        }
+        return !moves.isEmpty();
+    }
 
     @Override
     public String toString() {
