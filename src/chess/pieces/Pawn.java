a87339b3683ea716d424f24db61bf7be0c9cb00d
Alice <38640578+Alice434@users.noreply.github.com>
2019-03-24 19:03:08 -0400

    Update Pawn.java

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -13,12 +13,41 @@ public class Pawn extends BoardPiece {
         //image = PawnImage;
     }
 
+    //generate potential moves
     @Override
     public boolean getPotentialMoves(Board chess) {
-        return true;
-    }
+        moves.clear();
 
-    public boolean isValidMove(Point requestedMove) {return true;}
+        int x_pos = this.getCurrentSpace().getPosition().x;
+        int y_pos = this.getCurrentSpace().getPosition().y;
+
+        // loop through spaces in front of the piece
+        for (int i = y_pos; i < 8; i++) {
+            if (!checkForFriend(chess.board[i][x_pos])) {
+                moves.add(new Point(x_pos, i));
+            }
+        }
+
+        //TODO capture pieces diagonally
+        //moves forward-diagonally to capture pieces
+        for (int i = y_pos; i < 8; i++) {        //moving forward
+            if (!checkForFriend(chess.board[i][x_pos])) {
+                for (int j = x_pos; j >= 0; j--) {   //forward-left (left diagonal)
+                    if (!checkForFriend(chess.board[y_pos][j])) {
+                        moves.add(new Point(j, y_pos));
+                    }
+                    for (int k = x_pos; k < 8; k++) {   //forward-right (right diagonal)
+                        if (!checkForFriend(chess.board[y_pos][k])) {
+                            moves.add(new Point(k, y_pos));
+                        }
+                    }
+                }
+            }
+        }
+
+        // return false if piece has no move options
+        return !moves.isEmpty();
+    }
 
     @Override
     public String toString() {
