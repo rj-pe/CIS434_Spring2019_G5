7bdafad05aaf9fd08b6afdea54582b282931429d
Alex <36314268+gskairyu@users.noreply.github.com>
2019-04-14 17:01:06 -0400

    Create Graveyard.java

---
+++ src/boardlogic/Graveyard.java
@@ -0,0 +1,37 @@
+package boardlogic;
+
+import java.util.ArrayList;
+
+public class Graveyard {
+    //storage for Pieces
+    protected Team team;
+    protected ArrayList<BoardPiece> captured;
+
+    // Constructor
+
+    /**
+     * Creates a graveyard object to store the graveyard data in the captured array.
+     *
+     * @param t
+     */
+    public Graveyard(Team t) {
+        this.team = t;
+        this.captured = new ArrayList<>();
+    }
+
+    /**
+     * Adds a piece to the captured array.
+     * @param piece
+     */
+    public void addPiece(BoardPiece piece){
+        captured.add(piece);
+    }
+
+    /**
+     * Returns the captured ArrayList for the Graveyard Object.
+     * @return captured
+     */
+    public ArrayList<BoardPiece> getGraveyard(){
+        return captured;
+    }
+}
