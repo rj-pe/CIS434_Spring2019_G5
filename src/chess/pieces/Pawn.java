49ba0f4f02c79d1e890fbbe3e0328c31e5859b66
Alice <38640578+Alice434@users.noreply.github.com>
2019-04-17 14:38:24 -0400

    Update Pawn.java

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -27,23 +27,32 @@ public class Pawn extends BoardPiece {
         int y_p = y_pos + 1;    //down
         int x_m = x_pos - 1;    //left
         int y_m = y_pos - 1;    //up
+        int yw = y_pos + 2;  //WhP goes down moving up 2 spaces
+        int yb = y_pos - 2;  //BlP goes up moving up 2 spaces
 
         /*  no backward moves allowed
-        *   can move 1-2 spaces at a time
-        *   loop through spaces in front of the piece
-        */
-         if (team == WHITE) {
-            if (y_p < 8 && !Occupied(chess.board[y_p][x_pos])) 
+         *   loop through spaces in front of the piece
+         */
+        if (team == WHITE) {
+            if(y_pos == 1){ //can move upto 2 spaces forward if it's piece's first move
+                if (!Occupied(chess.board[yw][x_pos]))
+                    moves.add(new Point(x_pos, yw));
+            }
+            if (y_p < 8 && !Occupied(chess.board[y_p][x_pos]))
                 moves.add(new Point(x_pos, y_p));
         }
         else {     //for BlP moves only
+            if(y_pos == 6) { //can move upto 2 spaces forward if it's piece's first move
+                if (!Occupied(chess.board[yb][x_pos]))
+                    moves.add(new Point(x_pos, yb));
+            }
             if (y_m > 0 && !Occupied(chess.board[y_m][x_pos]))
                 moves.add(new Point(x_pos, y_m));
         }
-        
+
         /*  moves forward-diagonally to capture pieces  */
         // capture function done by Wh pieces (captures pieces in Q3 and Q4)
-        if(team == WHITE) {     
+        if(team == WHITE) {
             if (y_p < 8 && x_m > 0 && checkForEnemy(chess.board[y_p][x_m]) && !checkForFriend(chess.board[y_p][x_m])){
                 player.capture(chess.board[y_p][x_m].getOccupyingPiece());
                 moves.add(new Point(x_m, y_p));
@@ -64,14 +73,14 @@ public class Pawn extends BoardPiece {
                 moves.add(new Point(x_m, y_m));
             }
         }
-      
+
         // return false if piece has no move options
         return !moves.isEmpty();
     }
-    
+
     //TODO Promotion to Queen (or Knight or Rook or Bishop)
 
-    
+
     @Override
     public String toString() {
         return (team == WHITE) ? "WhP" : "BlP";
